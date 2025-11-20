342a3e683b79d7a945634b112a30c034133100c3
andre806 <megagamescanal6@gmail.com>
2025-11-19 21:06:17 -0300

    pronto

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "CupidoApp",
+  description: "conectando almas",
 };
 
 import PageIllustration from "@/components/page-illustration";
